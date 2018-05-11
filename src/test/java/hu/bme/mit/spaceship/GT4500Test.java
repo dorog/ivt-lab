45063098baac270f807c753e93a657e154a0626a
dorog <dorogi.x@gmail.com>
2018-05-11 04:50:30 -0700

    new file:   hu.bme.mit.spaceship.iml 	modified:   pom.xml 	modified:   src/main/java/hu/bme/mit/spaceship/GT4500.java 	modified:   src/test/java/hu/bme/mit/spaceship/GT4500Test.java

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,26 +9,147 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore prim;
+  private TorpedoStore sec;
 
   @Before
-  public void init(){
-    this.ship = new GT4500();
+  public void init()
+  {
+    prim = mock(TorpedoStore.class);
+    sec = mock(TorpedoStore.class);
+    this.ship = new GT4500(prim, sec);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(prim.isEmpty()).thenReturn(false);
+    when(sec.isEmpty()).thenReturn(false);
+    when(prim.fire(1)).thenReturn(true);
+    when(sec.fire(1)).thenReturn(true);
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(prim, times(1)).isEmpty();
+    verify(sec, times(0)).isEmpty();
+    verify(prim, times(1)).fire(1);
+    verify(sec, times(0)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_Only_prim(){
+    // Arrange
+    when(prim.isEmpty()).thenReturn(false);
+    when(sec.isEmpty()).thenReturn(true);
+    when(prim.fire(1)).thenReturn(true);
+    when(sec.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(prim, times(1)).isEmpty();
+    verify(sec, times(0)).isEmpty();
+    verify(prim, times(1)).fire(1);
+    verify(sec, times(0)).fire(1);
+
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_Only_second(){
+    // Arrange
+    when(prim.isEmpty()).thenReturn(true);
+    when(sec.isEmpty()).thenReturn(false);
+    when(prim.fire(1)).thenReturn(false);
+    when(sec.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(true, result);
+    verify(prim, times(1)).isEmpty();
+    verify(sec, times(1)).isEmpty();
+    verify(prim, times(0)).fire(1);
+    verify(sec, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(prim.isEmpty()).thenReturn(false);
+    when(sec.isEmpty()).thenReturn(false);
+    when(prim.fire(1)).thenReturn(true);
+    when(sec.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(true, result);
+    verify(prim, times(0)).fire(1);
+    verify(sec, times(1)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_All_Success_Only_prim(){
+    // Arrange
+    when(prim.isEmpty()).thenReturn(false);
+    when(sec.isEmpty()).thenReturn(true);
+    when(prim.fire(1)).thenReturn(true);
+    when(sec.fire(1)).thenReturn(false);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(true, result);
+    verify(prim, times(1)).fire(1);
+    verify(sec, times(1)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_Single_fire_twice_first_fire(){
+    // Arrange
+    when(prim.isEmpty()).thenReturn(false);
+    when(sec.isEmpty()).thenReturn(false);
+    when(prim.fire(1)).thenReturn(true);
+    when(sec.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    result = ship.fireTorpedo(FiringMode.SINGLE);;
+
+    // Assert
+    assertEquals(true, result);
+  }
+
+  @Test
+  public void fireTorpedo_Single_fire_twice_second_fire(){
+    // Arrange
+    when(prim.isEmpty()).thenReturn(false);
+    when(sec.isEmpty()).thenReturn(true);
+    when(prim.fire(1)).thenReturn(true);
+    when(sec.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    result = ship.fireTorpedo(FiringMode.SINGLE);;
+
+    // Assert
+    assertEquals(true, result);
+  }
+
+  @Test
+  public void fireTorpedo_All_Success_Only_sec(){
+    // Arrange
+    when(prim.isEmpty()).thenReturn(true);
+    when(sec.isEmpty()).thenReturn(false);
+    when(prim.fire(1)).thenReturn(false);
+    when(sec.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
